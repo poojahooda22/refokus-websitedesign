f94fcf05483d42989259c05b7e3af585e29de850
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:13:19 +0530

    updated anime

---
+++ components/header/anime.ts
@@ -0,0 +1,37 @@
+const transition = {duration: 1, ease: [0.76, 0, 0.24, 1]}
+
+export const opacity = {
+    initial: {
+        opacity: 0,
+    },
+    open: {
+        opacity: 1,
+        transition: {
+            duration: 0.35
+        }
+    },
+    closed: {
+        opacity: 0,
+        transition: {duration: 0.35}
+    }
+}
+
+
+export const menuSlide = {
+    initial: {x: "calc(100% + 100px)"},
+    enter: {x: "0", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
+    exit: {x: "calc(100% + 100px)", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}}
+}
+
+export const slide = {
+    initial: {x: 80},
+    enter: (i: number) => ({x: 0, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}}),
+    exit: (i: number) => ({x: 80, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}})
+}
+
+export const scale = {
+    open: {scale: 1, transition: {duration: 0.3}},
+    closed: {scale: 0, transition: {duration: 0.4}}
+}
+
+
