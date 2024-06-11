13bf1f97b6103d325d59f59fb9bcef2a5b97e8c5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:12:28 +0530

    work file

---
+++ app/work/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Header from '@/components/header/Index'
+import React from 'react'
+import WorkPage from '@/components/WorkPage'
+import Cursor from "@/components/StickyCursor/Index";
+import { useRef} from 'react';
+import Stripes from '@/components/Stripes';
+import Products from '@/components/Products';
+import Marquees from '@/components/Marquees';
+import Cards from '@/components/Cards';
+
+function Work() {
+    const stickyElement = useRef(null);
+  return (
+    <div className='w-full min-h-[100vh] relative bg-primary text-secondary overflow-hidden'>
+        <Cursor stickyElement={stickyElement} />
+        <Header/>
+        <WorkPage/>
+        <Stripes padding="py-[0vw]" />
+        <Products/>
+        <Marquees/>
+        <Cards/>
+    </div>
+  )
+}
+
+export default Work
