3869437dc97a0b50f05aa73044a1b2e5e510708e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:13:44 +0530

    updated index

---
+++ components/header/navbar/Index.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import React, {useState} from 'react';
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { menuSlide } from '../anime'
+import styles from './style.module.scss';
+import Link from './link/Index'
+import Curve from './Curve/Index'
+
+const navItems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Work",
+      href: "/work",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ]
+
+function Nav() {
+    const pathname = usePathname();
+    const [selectedIndicator, setSelectedIndicator] = useState(pathname);
+
+  return (
+    <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
+       <div className={styles.body}>
+            <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
+                    <div className={styles.header}>   
+                    </div>
+                    {
+                      navItems.map( (data, index) => {
+                        return <Link key={index} data={{...data, index}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
+                      })
+                    }
+            </div>
+            {/* <Footer /> */}
+        </div>
+        <Curve />
+    </motion.div>
+  )
+}
+
+export default Nav
