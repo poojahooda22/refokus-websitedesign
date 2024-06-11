a31ea00cad8e5ca9b312baddd5f76fe1ca67f8f4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 21:33:37 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1080px',
+      xl: '1840px',
+    },
+    colors: {
+      primary: '#111',
+      secondary: '#fff',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
