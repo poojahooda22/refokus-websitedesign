054340d52212df7772bdddb10f0ce26d7a9a91e2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:12:51 +0530

    cards

---
+++ components/Cards.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Card from './Card'
+
+function Cards() {
+  return (
+    <div className='w-full py-[4vw] sm:py-[2vw]'>
+        <div className='px-[5vw] sm:px-0 max-w-screen-xl mx-auto'>
+            <div className='flex flex-col-reverse gap-8 sm:flex sm:flex-row sm:gap-4 '>
+                <Card width="sm:w-1/3" 
+                    toptitle='Up Next: News' 
+                    toptitle2='Insights and behind the scenes'
+                    start={false} 
+                    para={true}
+                    
+                />
+                <Card 
+                    width="sm:w-2/3" 
+                    toptitle='Get In Touch' 
+                    toptitle2="Let's get to it, together."
+                    start={true}
+                    para={false}              
+                />
+            </div>
+            <div className='w-full sm:flex  items-center justify-between sm:mt-[2vw] mt-[6vw]'>
+                <div className='hidden w-full sm:flex items-center gap-2 sm:text-[.8vw] text-gray3'>
+                    <h5>Build with Nextjs, React</h5>
+                    <h5>Typescript</h5>
+                    <h5>GSAP, Locomotive-scroll</h5>
+                    <h5>Framer-motion</h5>
+                </div>
+                <h4 className='sm:text-[.9vw] text-purple'>Refokus@2024</h4>
+            </div>
+        </div>    
+    </div>
+  )
+}
+
+export default Cards
