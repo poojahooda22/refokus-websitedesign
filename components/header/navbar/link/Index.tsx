d0790d2917c6892199ea1d4dfe86cc9b6859d881
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:13:38 +0530

    created index

---
+++ components/header/navbar/link/Index.tsx
@@ -0,0 +1,16 @@
+import styles from './style.module.scss';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { slide, scale } from '../../anime';
+
+export default function Index({data, isActive, setSelectedIndicator}: {data: any, isActive: any, setSelectedIndicator: any}) {
+    
+        const { title, href, index} = data;
+    
+        return (
+            <motion.div className={styles.link} onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
+                <motion.div variants={scale} animate={isActive ? "open" : "closed"} className={styles.indicator}></motion.div>
+                <Link href={href}>{title}</Link>
+            </motion.div>
+        )
+    }
