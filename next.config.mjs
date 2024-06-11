42b5ffd48ff32091bb8d1b22b119e6995b23b29e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-11 22:16:08 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['assets-global.website-files.com'],
+    }, 
+};
 
 export default nextConfig;
