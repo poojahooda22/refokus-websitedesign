b4a838350a057d3c41582f40d9848d91af121d61
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-12 22:39:33 +0530

    updated contact

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -6,7 +6,8 @@ function ContactForm() {
     <div className='w-full relative'>
       <div className='max-w-screen-xl mx-auto pt-[8vw]'>
         <div className='w-1/2 min-h-[24vw] flex flex-col justify-between'>
-          <h1 className='text-[5vw] uppercase leading-none'>Don't Be Shy with us</h1>
+          <h1 className='text-[5vw] uppercase leading-none'>
+            Don't Be Shy with us</h1>
           <div>
             <p className='w-1/2 text-[.8vw] tracking-tight'>
               Don’t like forms? Well, too bad! This one is connected 
