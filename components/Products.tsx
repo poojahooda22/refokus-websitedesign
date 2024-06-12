1e5b8ee47b54f31731b1bf73f39e3b42c5bcb860
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-12 20:40:51 +0530

    updated products

--- components/Products.tsx
+++ components/Products.tsx
@@ -63,7 +63,17 @@ function Products() {
         <div className='absolute top-0 w-full h-full pointer-events-none'>
             <div className='absolute w-[24vw] h-[16vw] bg-gray3 left-[45%] -translate-x-1/2'>
                 
+                {images.map((elem, index) => (
+                    <Image 
+                        key={index}
+                        src={elem}
+                        className='w-full h-full'
+                        width={1000}
+                        height={100}
+                        alt="image"
+                    />
                 
+                ))}
                 
                 
                 
