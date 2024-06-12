b7dde729d114c84eaedf7267345edfce9b1f4779
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-12 22:41:28 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,7 +7,7 @@ function Card({width, toptitle, toptitle2, start, para}:
     ) {
   return (
     <div 
-        className={` ${width} min-h-[80vw] sm:min-h-[24vw] 
+        className={`${width} min-h-[80vw] sm:min-h-[24vw] 
             bg-gray rounded-lg sm:rounded-3xl
             py-[4vw] px-[4vw]
             sm:py-[1vw] sm:px-[1vw]
