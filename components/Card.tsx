0f8444a0c4ada2ad8f871d0374de76a6ed84ebfa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-12 22:42:31 +0530

    updated card

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,7 +17,7 @@ function Card({width, toptitle, toptitle2, start, para}:
             <div className='flex items-center justify-between mb-[1vw]'>
                 <p className='text-[3vw] sm:text-[.7vw]'>
                     {toptitle}
-                    </p>
+                </p>
                 <Image src='/arrow.svg' 
                     width={20} 
                     height={20} 
