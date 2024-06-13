29fb12a69734f2afd8a7856d70c303455e5b62e3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-13 23:34:52 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -9,7 +9,7 @@ function Form() {
                 <input
                     type="text"
                     placeholder="Name"
-                    className='w-full text-white p-4 rounded-xl'
+                    className='w-full bg-gray4 p-4 rounded-xl'
                 />
             </div>
             <div>
@@ -17,7 +17,7 @@ function Form() {
                 <input
                 type="text"
                 placeholder=''
-                className='w-full text-white p-4 rounded-xl' 
+                className='w-full  p-4 rounded-xl' 
                 />
             </div>
         </div>
