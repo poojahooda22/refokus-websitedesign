7928765df9b8cebccb3cab43d7fafe734b65658f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-13 22:24:15 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Form() {
   return (
-    <div className=''>
-      
+    <div className='w-[100%] h-[20vw] bg-black rounded-xl'>
+        <div>
+            <input
+                type="text"
+                placeholder="Name"
+                className='w-full text-white p-4 rounded-xl'
+            />
+        </div>
     </div>
   )
 }
