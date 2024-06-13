1af73ec8b065cd10b2524ac6c1a4c289e8de5df9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-13 21:52:42 +0530

    updated cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -24,7 +24,8 @@ function Cards() {
                 className='w-full sm:flex  items-center 
                 justify-between sm:mt-[2vw] mt-[6vw]'
             >
-                <div className='hidden w-full sm:flex 
+                <div 
+                className='hidden w-full sm:flex 
                     items-center gap-2 sm:text-[.8vw] 
                     text-gray3'
                 >
