91f5325b64080ebf42000f5ff4555ee588ca9dfa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-13 23:26:50 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -13,7 +13,9 @@ function Form() {
         </div>
         <div>
             <h4>Company</h4>
-            <input/>
+            <input
+                
+            />
         </div>
     </div>
   )
