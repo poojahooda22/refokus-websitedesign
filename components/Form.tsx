bad6e0b1437665226e8d7eb349768e7acf579973
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 22:41:20 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -34,7 +34,7 @@ function Form() {
             />
         </div>
         <div className='mt-[1vw]'>
-            <h4 className='text-[.7vw] mb-[.4vw]'>Email address</h4>
+            <h4 className='text-[.7vw] mb-[.4vw]'></h4>
             <input
                 type="text"
                 placeholder=''
