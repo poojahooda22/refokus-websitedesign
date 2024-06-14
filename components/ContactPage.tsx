3e3df4557dfa6e08ed8e7afbe09dd4508a9012ae
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 23:19:57 +0530

    added

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -8,24 +8,24 @@ function ContactForm() {
       <div className='max-w-screen-xl mx-auto pt-[8vw] flex justify-between'>
         <div>
           <div className='w-1/2 min-h-[20vw] flex flex-col justify-between bg-gray'>
-          <h1 className='text-[5vw] uppercase leading-none'>
-            Don't Be Shy with us
-          </h1>
-          <div>
-            <p className='w-1/2 text-[.8vw] tracking-tight'>
-              Don’t like forms? Well, too bad! This one is connected 
-              to our CRM and makes everything 
-              easier. Ok ok... here we go:
-            </p>
-            <button 
-              className='text-[3vw] sm:text-[.8vw]
-              py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
-              sm:py-[.5vw] rounded-full sm:mt-[1.5vw] 
-              border-[1px] border-secondary'
-            >
-              hello@refokus.com  
-            </button>
-          </div>  
+            <h1 className='text-[5vw] uppercase leading-none'>
+              Don't Be Shy with us
+            </h1>
+            <div>
+              <p className='w-1/2 text-[.8vw] tracking-tight'>
+                Don’t like forms? Well, too bad! This one is connected 
+                to our CRM and makes everything 
+                easier. Ok ok... here we go:
+              </p>
+              <button 
+                className='text-[3vw] sm:text-[.8vw]
+                py-[3vw] px-[5vw] sm:px-[1.3vw] mt-[4vw]
+                sm:py-[.5vw] rounded-full sm:mt-[1.5vw] 
+                border-[1px] border-secondary'
+              >
+                hello@refokus.com  
+              </button>
+            </div>  
           
           </div>
           <div className=' mt-[6vw]'>
