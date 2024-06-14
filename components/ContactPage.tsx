828237a47201df244acfee91cd761e0377ddb7b5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 23:19:23 +0530

    updated form

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -6,7 +6,8 @@ function ContactForm() {
   return (
     <div className='w-full relative'>
       <div className='max-w-screen-xl mx-auto pt-[8vw] flex justify-between'>
-        <div className='w-1/2 min-h-[20vw] flex flex-col justify-between '>
+        <div>
+        <div className='w-1/2 min-h-[20vw] flex flex-col justify-between bg-gray'>
           <h1 className='text-[5vw] uppercase leading-none'>
             Don't Be Shy with us
           </h1>
@@ -24,8 +25,14 @@ function ContactForm() {
             >
               hello@refokus.com  
             </button>
-          </div>        
+          </div>  
+          
         </div>
+          <div className=' mt-[6vw]'>
+          <Faq /> 
+          </div>
+        </div>
+        
         {/* //form */}
         <div className='w-1/3'>
           <Form />
