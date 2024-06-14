a725b53ee161134dbfb8a53f2bc18ee3ce9fbc13
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 22:20:36 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -14,7 +14,7 @@ function Form() {
                 <div>
                     <h4 className='text-[.7vw] mb-[.4vw]'>Company</h4>
                     <input
-                    type="text"
+                        type="text"
                 placeholder=''
                 className='w-full bg-gray4 p-4 rounded-xl' 
                 />
