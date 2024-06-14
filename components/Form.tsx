13b0e76c63aeb1af19438103e620d5636a5c92ea
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 22:28:04 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,13 +4,10 @@ function Form() {
   return (
     <div className='w-[100%] h-[20vw] bg-black rounded-xl py-[1vw] px-[2vw]'>
         <div className=''>
-            <div className=''>
-                <h4 className='text-[.7vw] mb-[.4vw]'>Full Name</h4>
-                <input
-                    type="text"
-                    placeholder=""
-                    className='w-full bg-gray4 p-4 rounded-xl'
-                />
+            <div className='flex'>
+                <div>
+                    
+                </div>
                 <div>
                     <h4 className='text-[.7vw] mb-[.4vw]'>Company</h4>
                     <input
