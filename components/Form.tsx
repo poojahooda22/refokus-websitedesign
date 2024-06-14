d86213e2d1e82cc05c41295eef3393cb67e94b8e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-14 22:19:30 +0530

    updated form

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Form() {
   return (
-    <div className='w-[100%] h-[20vw] bg-black rounded-xl py-[1vw] px-[0vw]'>
-        <div className='flex justify-evenly'>
+    <div className='w-[100%] h-[20vw] bg-black rounded-xl py-[1vw] px-[2vw]'>
+        <div className=''>
             <div>
                 <h4 className='text-[.7vw] mb-[.4vw]'>Full Name</h4>
                 <input
@@ -11,8 +11,7 @@ function Form() {
                     placeholder=""
                     className='w-full bg-gray4 p-4 rounded-xl'
                 />
-            </div>
-            <div>
+                <div>
                 <h4 className='text-[.7vw] mb-[.4vw]'>Company</h4>
                 <input
                 type="text"
@@ -20,6 +19,16 @@ function Form() {
                 className='w-full bg-gray4 p-4 rounded-xl' 
                 />
             </div>
+            </div>
+            
+        </div>
+        <div>
+            <h4 className='text-[.7vw] mb-[.4vw]'>Email address</h4>
+            <input
+                type="text"
+                placeholder=''
+                className='w-full bg-gray4 p-4 rounded-xl' 
+            />
         </div>
         
     </div>
