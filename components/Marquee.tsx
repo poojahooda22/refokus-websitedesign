bffee9904b4dccafed1bd2372c776c20f22eff8a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 13:51:19 +0530

    updated

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 function Marquee({imagesurl}: {imagesurl: any}) {
     return (
         <div className='w-full flex items-center gap-[10vw] sm:gap-[6vw] py-[1.5vw] overflow-hidden'>
+            <div>
             {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
+            </div>
+            
             {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
         </div>
     )
