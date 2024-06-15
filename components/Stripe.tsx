d9c6a7a7fff4fb85cba5f195aafbb73bda462fc4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 14:16:38 +0530

    added

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -4,14 +4,22 @@ import { motion } from 'framer-motion'
 
 function Stripe({val}: {val: any}) {
     return (
-        <div 
-            className='relative w-[70%] sm:w-[16.66%] sm:px-[1vw] 
-            px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
-            border-b-[1px] border-[#333] flex justify-between items-center'
-        >
-            <Image src={val.url} width={1000} height={100} alt="" className='w-[24vw] sm:w-[7vw] '/>
-            <span className=' sm:text-[1vw] font-semibold'>{val.number}</span>
-        </div>
+        <>
+            <motion.div 
+                initial={{ x: '0%'}}
+                animate={{ x: '-100%'}}
+                transition={{ease: 'linear', duration: 20, repeat: Infinity}}
+                className='relative w-[70%] sm:w-[16.66%] sm:px-[1vw] 
+                px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
+                border-b-[1px] border-[#333] flex justify-between items-center'
+            >
+                <Image src={val.url} width={1000} height={100} alt="" className='w-[24vw] sm:w-[7vw] '/>
+                <span className=' sm:text-[1vw] font-semibold'>{val.number}</span>
+                
+            </motion.div>
+        </>
+        
+        
     )
 }
 
