c1bb9475909427451d7bb442c1ded30776218212
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 14:13:34 +0530

    updated marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -2,20 +2,27 @@ import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
 
-function Marquee({imagesurl}: {imagesurl: any}) {
+function Marquee({imagesurl, direction}: {imagesurl: any, direction: any}) {
     return (
         <div className='w-full flex items-center gap-[10vw] sm:gap-[6vw] py-[1.5vw] overflow-hidden'>
             <motion.div 
-                initial={{ x: 0}}
-                animate={{ x: "-100%"}}
-                transition={{ease: 'linear', duration: 10, repeat: Infinity, repeatType: 'loop'}}
+                initial={{ x: direction === 'left' ? '0' : '-100%'}}
+                animate={{ x: direction === 'left' ? '-100%' : '0'}}
+                transition={{ease: 'linear', duration: 30, repeat: Infinity}}
                 className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
                 {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
+                
             </motion.div>
 
-            <motion.div className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
+            <motion.div 
+                initial={{ x: direction === 'left' ? '0' : '-100%'}}
+                animate={{ x: direction === 'left' ? '-100%' : '0'}}
+                transition={{ease: 'linear', duration: 30, repeat: Infinity}}
+                className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
                 {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
+                
             </motion.div>
+
             
             
         </div>
