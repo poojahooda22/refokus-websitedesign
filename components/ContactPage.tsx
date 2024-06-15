3455afd0d524a7a5feb69b2052282de5664fedaf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 15:19:05 +0530

    updated contact

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -5,14 +5,15 @@ import Form from './Form'
 function ContactForm() {
   return (
     <div className='w-full relative'>
-      <div className='max-w-screen-xl mx-auto pt-[8vw] flex justify-between'>
-        <div className='w-1/2'>
+      <div className=' max-w-screen-xl mx-auto pt-[8vw] px-[5vw] sm:px-0 mt-[10vw] 
+        sm:mt-[0vw]  flex flex-col-reverse sm:flex sm:flex-row justify-between'>
+        <div className='w-full sm:w-1/2'>
           <div className='w-full min-h-[20vw] flex flex-col justify-between '>
-            <h1 className='text-[5vw] uppercase leading-none'>
+            <h1 className='hidden sm:inline-block text-[5vw] uppercase leading-none'>
               Don't Be Shy with us
             </h1>
             <div>
-              <p className='w-1/2 text-[.8vw] tracking-tight'>
+              <p className='sm:w-1/2 text-[.8vw] tracking-tight'>
                 Don’t like forms? Well, too bad! This one is connected 
                 to our CRM and makes everything 
                 easier. Ok ok... here we go:
@@ -33,7 +34,7 @@ function ContactForm() {
         </div>
         
         {/* //form */}
-        <div className='w-1/3'>
+        <div className='sm:w-1/3 '>
           <Form />
         </div>
       </div>
