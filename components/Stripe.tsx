30f99c4debe5070d1fd0298e271c0e48faf12842
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 14:29:20 +0530

    updated stripe

--- components/Stripe.tsx
+++ components/Stripe.tsx
@@ -6,9 +6,9 @@ function Stripe({val}: {val: any}) {
     return (
         <>
             <motion.div 
-                initial={{ x: '0%'}}
-                animate={{ x: '-100%'}}
-                transition={{ease: 'linear', duration: 20, repeat: Infinity}}
+                // initial={{ x: '0%'}}
+                // animate={{ x: '-100%'}}
+                // transition={{ease: 'linear', duration: 20, repeat: Infinity}}
                 className='relative w-[70%] sm:w-[16.66%] sm:px-[1vw] 
                 px-[4vw] py-[2vw] sm:py-[1vw] border-r-[1px] border-t-[1px]
                 border-b-[1px] border-[#333] flex justify-between items-center'
