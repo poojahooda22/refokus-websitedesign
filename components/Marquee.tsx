8ef54eaf1491e499d44b98b9e693c410e71ef88a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-15 13:57:55 +0530

    updated marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Marquee({imagesurl}: {imagesurl: any}) {
     return (
         <div className='w-full flex items-center gap-[10vw] sm:gap-[6vw] py-[1.5vw] overflow-hidden'>
-            <div className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
+            <motion.div 
+                className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
                 {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
-            </div>
+            </motion.div>
 
-            <div className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
+            <motion.div className='flex flex-shrink-0 gap-[10vw] sm:gap-[6vw]'>
                 {imagesurl.map((url: string) => <Image width={1000} height={100}  src={url} className='w-[20vw] sm:w-[6vw] flex-shrink-0' alt=""/>)}
-            </div>
+            </motion.div>
             
             
         </div>
