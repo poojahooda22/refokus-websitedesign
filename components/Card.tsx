ef06cd339c25285058cb30de2cfaeadee9887578
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-16 21:27:21 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,7 +13,7 @@ function Card({width, toptitle, toptitle2, start, para, hover="false"}:
         bg-gray rounded-lg sm:rounded-3xl
         py-[4vw] px-[4vw]
         sm:py-[1vw] sm:px-[1vw]
-            flex flex-col justify-between`}
+        flex flex-col justify-between`}
     >
         <div className='w-full'>
             <div className='flex items-center justify-between mb-[1vw]'>
