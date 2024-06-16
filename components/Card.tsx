906157361c6d0f12437a0a69a8da89af173a61ec
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-16 21:30:36 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -45,7 +45,7 @@ function Card({width, toptitle, toptitle2, start, para, hover="false"}:
             <div className='w-1/2 pt-[2vw] sm:pt-0'>
                 <h3 
                     className='text-[4.6vw] 
-                font-regular sm:text-[1.2vw]'>
+                    font-regular sm:text-[1.2vw]'>
                     {toptitle2}
                 </h3>
             </div>    
