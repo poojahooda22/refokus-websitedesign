89b8bdb82e417ae81dd0f5b1e095d9b202d97915
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-16 21:28:46 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -25,7 +25,8 @@ function Card({width, toptitle, toptitle2, start, para, hover="false"}:
     >
         <div className='w-full'>
             <div 
-                className='flex items-center justify-between mb-[1vw]'>
+                className='flex items-center 
+                justify-between mb-[1vw]'>
                 <p className='text-[3vw] sm:text-[.7vw]'>
                     {toptitle}
                 </p>
