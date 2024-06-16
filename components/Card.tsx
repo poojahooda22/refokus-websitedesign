9502437d978c1370c780a11f683036a347ab9429
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-16 21:28:16 +0530

    added

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,7 +16,8 @@ function Card({width, toptitle, toptitle2, start, para, hover="false"}:
   return (
     <motion.div 
         whileHover={{ backgroundColor: hover === true && '#7443ff', padding: '28px'}}
-        className={`${width} min-h-[80vw] sm:min-h-[24vw] 
+        className={`${width} min-h-[80vw] 
+            sm:min-h-[24vw] 
         bg-gray rounded-lg sm:rounded-3xl
         py-[4vw] px-[4vw]
         sm:py-[1vw] sm:px-[1vw]
