33b74303205fd459f6c4a5ad3526e06543327129
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-16 21:26:33 +0530

    added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className='w-screen relative pt-[8vw]'>
       <div 
-      className=' max-w-screen-xl mx-auto 
+        className=' max-w-screen-xl mx-auto 
         flex flex-col gap-[10vw] px-[5vw] 
         xl:px-0 mt-[20vw] 
         sm:mt-[6vw] lg:mt-[6vw] xl:mt-0'
