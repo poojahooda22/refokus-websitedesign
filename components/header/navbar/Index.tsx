82a518228a6d28d865d4ae2bf5a5f58b41f81793
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-23 21:13:04 +0530

    added

--- components/header/navbar/Index.tsx
+++ components/header/navbar/Index.tsx
@@ -43,7 +43,7 @@ function Nav() {
                       })
                     }
             </div>
-            {/* <Foe /> */}
+            {/* <Fo /> */}
         </div>
         <Curve />
     </motion.div>
