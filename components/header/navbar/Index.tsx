78f206a3e02cb9789c2d252b52e6f1f8a854aa32
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-23 21:17:30 +0530

    added

--- components/header/navbar/Index.tsx
+++ components/header/navbar/Index.tsx
@@ -35,8 +35,7 @@ function Nav() {
     <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
       <div className={styles.body}>
         <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
-          <div className={styles.header}>   
-            </div>
+          <div className={styles.header}>   </div>
               {
                       navItems.map( (data, index) => {
                         return <Link key={index} data={{...data, index}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
