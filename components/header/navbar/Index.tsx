d5dc88f5999b14022e8d4bb1783f0e0160163350
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-23 21:14:53 +0530

    added

--- components/header/navbar/Index.tsx
+++ components/header/navbar/Index.tsx
@@ -34,7 +34,7 @@ function Nav() {
   return (
     <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
        <div className={styles.body}>
-            <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
+          <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
                 <div className={styles.header}>   
                     </div>
                     {
