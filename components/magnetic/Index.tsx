aa2a4572474561f4172807d2d743c22745160dee
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-03 17:34:29 +0530

    updated magnetic

--- components/magnetic/Index.tsx
+++ components/magnetic/Index.tsx
@@ -3,13 +3,13 @@ import { useRef, useState } from "react";
 import {motion} from "framer-motion";
 
 export default function Framer({children}: any) {
-    const ref = useRef(null);
+    const ref = useRef<HTMLDivElement>(null);
 
     const [ position, setPosition] = useState({x:0, y:0});
 
-    const handleMouse = (e) => {
+    const handleMouse = (e: any) => {
         const { clientX, clientY} = e;
-        const { height, width, left, top} = ref.current.getBoundingClientRect();
+        const { height, width, left, top} = (ref.current as HTMLElement)!.getBoundingClientRect();
         const middleX = clientX - (left + width/2)
         const middleY = clientY - (top + height /2)
         setPosition({x: middleX * 0.1, y: middleY * 0.1})
