88a1be044dc0ef527f8c12ab686d45c18d997161
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-03 21:30:48 +0530

    added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,7 +13,7 @@ function Contact() {
         className='w-full min-h-[100vh] relative 
       bg-primary text-secondary overflow-hidden'
     >
-      {/* <Cursor stickyElement={stickyElemen>*/}
+      {/* <Cursor stickyElement={stickyElem>*/}
       <Header/>
       <ContactForm/>
     </div>
