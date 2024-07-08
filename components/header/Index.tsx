657fa5385b45c77e8d950301a46c987131d9582d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-08 21:52:29 +0530

    added

--- components/header/Index.tsx
+++ components/header/Index.tsx
@@ -72,7 +72,7 @@ const Header = forwardRef<HTMLDivElement, IndexProps>(function Index(props, ref)
                         })}
                     </div>   
                 </div>
-                {/* <i'ie'><Buttn/</div*/}
+                {/* <i'i'><Buttn/</div*/}
                 <Magnetic>
                     <div onClick={() => {setIsActive(!isActive)}} className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}>
                         <div ref={ref} className={styles.bounds}></div>
