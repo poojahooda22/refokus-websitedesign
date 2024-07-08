d6008783d61ae2302ec421b83d69879406981815
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-08 21:47:03 +0530

    added

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import React from 'react'
 
 function Button({text, border, borderCol, bg, txt}: 
-  {text: string, border?: string, borderCol?: string, bg?: string, txt?: string}) 
+  {text: string, 
+    border?: string, borderCol?: string, bg?: string, txt?: string}) 
   {
   return (
     <div className={`w-fit px-[6vw] py-[3vw] sm:px-[1vw] sm:py-[.3vw] ${bg} ${border} ${borderCol}
