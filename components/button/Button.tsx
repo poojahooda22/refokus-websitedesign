56c2557c6f9fcbe856cefc7e06ac214ef4402f5a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-08 21:47:28 +0530

    added

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -6,7 +6,8 @@ function Button({text, border, borderCol, bg, txt}:
     text: string, 
     border?: string, 
     borderCol?: string, 
-    bg?: string, txt?: string}) 
+    bg?: string, 
+    txt?: string}) 
   {
   return (
     <div className={`w-fit px-[6vw] py-[3vw] sm:px-[1vw] sm:py-[.3vw] ${bg} ${border} ${borderCol}
