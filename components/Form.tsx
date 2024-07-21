fd63c9ee4cf69f1e8b1a1d37707d78c646ed29e4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-21 20:58:03 +0530

    added

--- components/Form.tsx
+++ components/Form.tsx
@@ -75,7 +75,8 @@ function Form() {
                     className='text-[3vw] sm:text-[.7vw] 
                     mb-[2vw] sm:mb-[.4vw]'
                 >
-                    What&apos;s on your mind</h4>
+                    What&apos;s on your mind
+                    </h4>
                 <textarea
                     name="message"
                     id="message"
