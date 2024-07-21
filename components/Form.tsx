7996e68279694972c72fed60e7a8d4e5ddc30eaa
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-21 20:53:29 +0530

    added

--- components/Form.tsx
+++ components/Form.tsx
@@ -40,7 +40,8 @@ function Form() {
                 <h4 className='text-[3vw] sm:text-[.7vw] 
                     mb-[2vw] sm:mb-[.4vw]'
                 >
-                    Email address</h4>
+                    Email address
+                    </h4>
                 <input
                     type="text"
                     placeholder=''
