3943f89eb09a4f63d28898731efdac4f15749385
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-21 20:51:46 +0530

    added

--- components/Form.tsx
+++ components/Form.tsx
@@ -20,7 +20,8 @@ function Form() {
                     />
                 </div>
                 <div>
-                    <h4 className='text-[3vw] sm:text-[.7vw] mb-[2vw] sm:mb-[.4vw]'>Company</h4>
+                    <h4 className='text-[3vw] sm:text-[.7vw] 
+                    mb-[2vw] sm:mb-[.4vw]'>Company</h4>
                     <input
                         type="text"
                         placeholder=''
