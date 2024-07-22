b1043d88c9f1a4b99df75e3ff701e6f2c44109a7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-22 21:12:20 +0530

    comment out the images

--- components/WorkPage.tsx
+++ components/WorkPage.tsx
@@ -7,13 +7,13 @@ import gsap, { Power2, Power3 } from 'gsap'
 
 function WorkPage() {
 
-  const images = [
-    'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png',
-    'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png',
-    'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png',
-    'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png',
-    'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png'
-  ]
+  // const images = [
+  //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png',
+  //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png',
+  //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png',
+  //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png',
+  //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png'
+  // ]
 
   // const [images, setImages] = useState(
   //   [
