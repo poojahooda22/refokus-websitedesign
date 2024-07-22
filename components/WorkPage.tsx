f00fafd3547dfd5a003a2ff72867b7193166898f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-22 21:27:57 +0530

    added

--- components/WorkPage.tsx
+++ components/WorkPage.tsx
@@ -8,7 +8,7 @@ import gsap, { Power2, Power3 } from 'gsap'
 function WorkPage() {
 
   // const images = [
-  //   'https://assets-global.website-files.com/,
+  //   'https://assets-global.website-file/,
   // ]
 
   // const [images, setImages] = useState(
