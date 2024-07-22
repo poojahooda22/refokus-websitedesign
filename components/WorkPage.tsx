62b84d03c2522c37f0f48ea8d07a686ace37d615
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-22 21:18:01 +0530

    updated work page

--- components/WorkPage.tsx
+++ components/WorkPage.tsx
@@ -11,7 +11,7 @@ function WorkPage() {
   //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png',
   //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png',
   //   'https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png',
-  //   'https://assets-global.website-files.com
+  //   'https://assets-global.
   //   '
   // ]
 
